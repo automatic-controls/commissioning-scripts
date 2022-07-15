31200074848a02806f5245b6362c1289e2114586
Cameron Vogt <cvogt729@gmail.com>
2022-07-15 16:24:57 -0500

    worked on mapping webpages

---
+++ src/aces/webctrl/scripts/commissioning/web/MappingEditPage.java
@@ -0,0 +1,9 @@
+package aces.webctrl.scripts.commissioning.web;
+import aces.webctrl.scripts.commissioning.core.*;
+import javax.servlet.http.*;
+import java.util.*;
+public class MappingEditPage extends ServletBase {
+  @Override public void exec(final HttpServletRequest req, final HttpServletResponse res) throws Throwable {
+    //TODO
+  }
+}
