77d32f39b0b3af3865aa8b40a0c09a3200c18788
Cameron Vogt <cvogt729@gmail.com>
2022-08-15 13:54:21 -0500

    remove suffix from dev-jars

--- ext/archive.bat
+++ ext/archive.bat
@@ -2,6 +2,6 @@ if /i "%*" EQU "--help" (
   echo ARCHIVE           Packs source files into a .jar archive.
   exit /b 0
 )
-"%JDKBin%\jar.exe" -c -M -f "%workspace%\!name!-!projectVersion!-sources.jar" -C "%src%" .
-"%JDKBin%\jar.exe" -c -M -f "%workspace%\!name!-!projectVersion!.jar" -C "%classes%" .
+"%JDKBin%\jar.exe" -c -M -f "%workspace%\!name!-sources.jar" -C "%src%" .
+"%JDKBin%\jar.exe" -c -M -f "%workspace%\!name!.jar" -C "%classes%" .
 exit /b %ERRORLEVEL%
