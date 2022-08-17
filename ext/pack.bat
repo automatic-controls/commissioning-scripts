fce07a289e1a77bf3d2d1692c28cd257d2a95ee4
Cameron Vogt <cvogt729@gmail.com>
2022-08-17 11:42:57 -0500

    update build script

---
+++ ext/pack.bat
@@ -0,0 +1,26 @@
+if /i "%*" EQU "--help" (
+  echo PACK              Packages all relevant files into newly created .addon and .jar archives.
+  exit /b 0
+)
+setlocal
+  set "override=0"
+  call "%callback%" --goto pack
+  set "err=%ErrorLevel%"
+endlocal & set "err=%err%"
+echo Archiving...
+set "sourceJar=%workspace%\!name!-sources.jar"
+set "classJar=%workspace%\!name!.jar"
+if exist "%sourceJar%" del /F "%sourceJar%" >nul 2>nul
+if exist "%classJar%" del /F "%classJar%" >nul 2>nul
+set "err2=0"
+"%JDKBin%\jar.exe" -c -M -f "%sourceJar%" -C "%src%" .
+if %ErrorLevel% NEQ 0 set "err2=1"
+"%JDKBin%\jar.exe" -c -M -f "%classJar%" -C "%classes%" .
+if %ErrorLevel% NEQ 0 set "err2=1"
+if "%err2%" EQU "0" (
+  echo Archive successful.
+) else (
+  echo Archive unsuccessful.
+  exit /b %err2%
+)
+exit /b %err%
